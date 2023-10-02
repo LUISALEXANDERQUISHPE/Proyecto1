a88591c1ad5308937406efac30d546147dfe1273
Ismael <ismaelsailema110@gmail.com>
2023-10-01 23:52:14 -0500

    Getters y Setters en Persona

--- src/Persona.java
+++ src/Persona.java
@@ -27,6 +27,56 @@ class Persona {
         this.FechaNacmto = FechaNacmto;
     }
 
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre1() {
+        return nombre1;
+    }
+
+    public void setNombre1(String nombre1) {
+        this.nombre1 = nombre1;
+    }
+
+    public String getNombre2() {
+        return nombre2;
+    }
+
+    public void setNombre2(String nombre2) {
+        this.nombre2 = nombre2;
+    }
+
+    public String getApellido1() {
+        return Apellido1;
+    }
+
+    public void setApellido1(String Apellido1) {
+        this.Apellido1 = Apellido1;
+    }
+
+    public String getApellido2() {
+        return Apellido2;
+    }
+
+    public void setApellido2(String Apellido2) {
+        this.Apellido2 = Apellido2;
+    }
+
+    public int getFechaNacmto() {
+        return FechaNacmto;
+    }
+
+    public void setFechaNacmto(int FechaNacmto) {
+        this.FechaNacmto = FechaNacmto;
+    }
+    
+    
+
     public int Edad(int anioP, int mesP, int diaP) {
         Date fecha = new Date();
         int dia, mes, anio, anioCal;
